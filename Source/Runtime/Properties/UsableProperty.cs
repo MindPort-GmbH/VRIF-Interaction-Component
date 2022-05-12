bdddb59fe4eb01e4de176bc12df75fc315b35fb4
Marcello Tridenti <marcello.tridenti@tutanota.com>
2022-05-12 18:21:58 +0200

    Created Usable Property

---
+++ Source/Runtime/Properties/UsableProperty.cs
@@ -0,0 +1,76 @@
+using BNG;
+using System;
+using UnityEngine;
+using VRBuilder.BasicInteraction.Properties;
+using VRBuilder.Core.Properties;
+
+namespace VRBuilder.VRIF.Properties
+{
+    [AddComponentMenu("VR Builder/Properties/Interhaptics/Usable Property (VRIF)")]
+    [RequireComponent(typeof(GrabbableUnityEvents))]
+    public class UsableProperty : LockableProperty, IUsableProperty
+    {
+        public bool IsBeingUsed => throw new NotImplementedException();
+
+        public event EventHandler<EventArgs> UsageStarted;
+        public event EventHandler<EventArgs> UsageStopped;
+
+        private Grabbable grabbable;
+        private GrabbableUnityEvents grabbableEvents;
+
+        protected Grabbable Grabbable
+        {
+            get
+            {
+                if (grabbable == null)
+                {
+                    grabbable = GetComponent<Grabbable>();
+                }
+
+                return grabbable;
+            }
+        }
+
+        protected GrabbableUnityEvents GrabbableEvents
+        {
+            get
+            {
+                if (grabbableEvents == null)
+                {
+                    grabbableEvents = GetComponent<GrabbableUnityEvents>();
+                }
+
+                return grabbableEvents;
+            }
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            GrabbableEvents.onTriggerDown.AddListener(HandleUsed);
+
+            InternalSetLocked(IsLocked);
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+            GrabbableEvents.onTriggerDown.RemoveListener(HandleUsed);
+        }
+
+        private void HandleUsed()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FastForwardUse()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void InternalSetLocked(bool lockState)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
